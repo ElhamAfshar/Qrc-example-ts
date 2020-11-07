c154cdbde9ca2b7798e00d16eb2176ccb0da0116
MissNull <elhamafshar2100@gmail.com>
2020-11-07 13:47:07 +0330

    first cmt

---
+++ models/user.ts
@@ -0,0 +1,35 @@
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+export const userSchema = new Schema({
+    fristName :String,
+    lastName : String,
+    ssn:String,
+    phoneNumber:{type:String,unique:true},
+    phoneNumberValidate: { type: Boolean, default: false },
+    email: { type: String, lowercase: true },
+    image:String, /// ???
+    authCode:Number,
+    level: [
+        {
+          type: String,
+          enum: [
+           "superAdmin",
+           "admin",
+           "centerAdmin",
+           "guest",
+           "normal"
+          ],
+          default: ["guest"]
+        }
+    ],
+    guestUserId:String
+    
+
+},
+{ timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
+
+const ModelClass = mongoose.model('User', userSchema);
+
+module.exports = ModelClass;
