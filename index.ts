67f6e9cbf772320449731393f6b893799dcde139
zsattari <famil.shia14@gmail.com>
2020-11-08 05:17:18 -0500

    ntn

--- index.ts
+++ index.ts
@@ -1,19 +1,16 @@
-import Express from "express"
-import * as mongoose from "mongoose"
-import {router} from "./routes/route"
-import Redis from "ioredis"
+import Express from "express";
+import mongoose from "mongoose";
+import { router } from "./routes/route";
+import Redis from "ioredis";
 
-
-
-mongoose.connect('mongodb://localhost/QRC', {useNewUrlParser: true});
-const app =Express()
+mongoose.connect("mongodb://localhost/QRC", { useNewUrlParser: true });
+const app = Express();
 export const clientRedis = new Redis();
- 
-const port =3000;
+
+const port = 3000;
 
 router(app);
 
-app.listen(port,()=>{
-    console.log("strat server ...");
-    
-})
+app.listen(port, () => {
+  console.log("strat server ...");
+});
--- routes/route.ts
+++ routes/route.ts
@@ -1,9 +1,9 @@
-import {Application,Request,Response} from "express"
-import bodyParser from "body-parser"
-import * as api from "../controllers/Authentication"
+import { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import * as api from "../controllers/Authentication";
 
-export const router= (app:Application)=>{
-    const parser=bodyParser.json();
-    app.route('/req-code').post(parser,api.reqcode);
-    app.route('/get-token').post(parser,api.reqcode);
+export const router = (app: Application) => {
+  const parser = bodyParser.json();
+  app.route("/req-code").post(parser, api.reqcode);
+  app.route("/get-token").post(parser, api.reqcode);
 };
